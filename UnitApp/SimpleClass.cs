095ec10c2e53f1c5cb5d911ef84afc0942ec2610
Pavel <99116523+TestingInfo@users.noreply.github.com>
2022-09-09 14:29:31 +0300

    Update SimpleClass.cs

--- UnitApp/SimpleClass.cs
+++ UnitApp/SimpleClass.cs
@@ -1,5 +1,3 @@
-﻿using System;
-
 namespace UnitApp
 {
     public class SimpleClass
@@ -14,7 +12,31 @@ namespace UnitApp
 
             return false;
         }
+        
+        public static bool IsLengthLessMaxInteger(string str)
+        {
+            int intLength = Int32.MaxValue.ToString().Length;
+            // 2,147,483,647
+            if (str.Length >= intLength - 1)
+            {
+                Console.WriteLine($"Вы ввели сильно длинное значение. Больше, чем {intLength - 1}");
+                return false;
+            }
+            return true;
+        }
 
+        public static bool OloloInString(string str)
+        {
+            string a;
+            string b;
+            string c;
+            if (str == "ololo")
+            {
+                return true;
+            }
+            return false;
+        }
+        
         public static bool IsDigital(string str)
         {
             foreach (char ch in str)
@@ -28,17 +50,7 @@ namespace UnitApp
             return true;
         }
 
-        public static bool IsLengthLessMaxInteger(string str)
-        {
-            int intLength = Int32.MaxValue.ToString().Length;
-            // 2,147,483,647
-            if (str.Length >= intLength - 1)
-            {
-                Console.WriteLine($"Вы ввели сильно длинное значение. Больше, чем {intLength - 1}");
-                return false;
-            }
-            return true;
-        }
+
 
         public static string CheckString()
         {
@@ -47,9 +59,34 @@ namespace UnitApp
             {
                 Console.WriteLine("Введите строку");
                 a = Console.ReadLine();
-            } while (IsEmpty(a) || !IsDigital(a) || !IsLengthLessMaxInteger(a));
-
+            } while (IsEmpty(a) || !IsLengthLessMaxInteger(a) || OloloInString(a));
+                Console.WriteLine("Печатаем вашу строку консоль " + a);
             return a;
+        }
+
+        public static void Prog()
+        {
+            Console.WriteLine("Веедите строку");
+            string str = Console.ReadLine();
+            
+            string a;
+            string b;
+            string c;
+            
+            if (OloloInString(str))
+            {
+                Console.WriteLine("Введите первое число");
+                a = Console.ReadLine();
+                Console.WriteLine("Введите второе число");
+                b = Console.ReadLine();
+                if(!IsEmpty(a) || !IsEmpty(b) || !IsLengthLessMaxInteger(a) || !IsLengthLessMaxInteger(b) || IsDigital(a) || IsDigital(b))
+                {
+                     c = Convert.ToString((Convert.ToInt32(a)) + (Convert.ToInt32(b)));
+                                    Console.WriteLine("Сумма ваших чисел" + c);
+                                    
+                }
+               
+            }Console.WriteLine("Печатаем вашу строку консоль " + str);
         }
     }
 }
