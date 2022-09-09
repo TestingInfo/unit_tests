1a059e1825ce8fe6abd7018c37329b3961f5ea80
Pavel <99116523+TestingInfo@users.noreply.github.com>
2022-09-09 14:44:00 +0300

    Update Program.cs

--- UnitApp/Program.cs
+++ UnitApp/Program.cs
@@ -1,6 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
 using System;
 using UnitApp;
 
-SimpleClass.CheckString();
+//SimpleClass.CheckString();
+SimpleClass.Prog();
